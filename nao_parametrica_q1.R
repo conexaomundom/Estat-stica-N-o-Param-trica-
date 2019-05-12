d2b6cb527bdad19419e409763cce428f03bc57e5
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-05-12 15:11:37 -0300

    Verificação por MC e por MC com Bootstrap.

--- nao_parametrica_q1.R
+++ nao_parametrica_q1.R
@@ -7,31 +7,74 @@
 # Tamanho da amostra.
 n <- 30
 # Quantas repetições a simulação vai ter.
-k <- 10000
+k <- 1000
 B <- 250
+
+###############################################################################
+# # Apenas com Monte Carlo.
+###############################################################################
 vetor1 <- NULL
 vetor2 <- NULL
 vetor3 <- NULL
 
 for(i in 1:k){
+  x <- rnorm(n, mean = 0, sd = 1)
+    est <- wilcox.test(x, alternative = "two.sided")
+    
+    vetorp[i] <- est$p.value
+    
+    vetor1[i] <- ifelse(est$p.value < 0.1,1,0)
+    vetor2[i] <- ifelse(est$p.value < 0.05 ,1,0)
+    vetor3[i] <- ifelse(est$p.value < 0.01,1,0)
+}
+meanp <- mean(vetorp)
+soma1 <- sum(vetor1)
+soma2 <- sum(vetor2)
+soma3 <- sum(vetor3)
+soma4 <- sum(vetor1,vetor2,vetor3)
+
+
+###############################################################################
+# Monte Carlo com bootstrap.
+################################################################################
+soma1 <- NULL
+soma2 <- NULL
+soma3 <- NULL
+soma4 <-NULL
+
+meanp <- NULL
+
+
+for(i in 1:k){
   vetor1 <- NULL
   vetor2 <- NULL
   vetor3 <- NULL
+  vetorp <- NULL
   x <- rnorm(n, mean = 0, sd = 1)
-  for(i in 1:B){
+  for(j in 1:B){
    amostra <-  sample(x,length(x),replace = TRUE)
   est <- wilcox.test(amostra, alternative = "two.sided")
-  vetor1[i] <- ifelse(est$p.value < 0.1,1,0)
-  vetor2[i] <- ifelse(est$p.value < 0.05 ,1,0)
-  vetor3[i] <- ifelse(est$p.value < 0.01,1,0)
+  
+  vetorp[j] <- est$p.value
+  
+  vetor1[j] <- ifelse(est$p.value < 0.1,1,0)
+  vetor2[j] <- ifelse(est$p.value < 0.05,1,0)
+  vetor3[j] <- ifelse(est$p.value < 0.01,1,0)
   }
   
-  sum(vetor1)
-  sum(vetor2)
-  sum(vetor3)
-  sum(vetor1,vetor2,vetor3)
+  meanp[i] <- mean(vetorp)
+  soma1[i] <- sum(vetor1)
+  soma2[i] <- sum(vetor2)
+  soma3[i] <- sum(vetor3)
+  soma4[i] <- sum(vetor1,vetor2,vetor3)
 }
-sum(vetor1)
-sum(vetor2)
-sum(vetor3)
-sum(vetor1,vetor2,vetor3)
+
+mean(meanp)
+mean(soma1)
+mean(soma2)
+mean(soma3)
+mean(soma4)
+
+###############################################################################
+# Monte Carlo com bootstrap, para estimar o erro padrão.
+###############################################################################
